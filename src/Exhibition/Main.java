5b26589c7903c2923177450f54f48136c2309123
vu-group10 <ugfirst22@gmail.com>
2025-06-19 20:27:56 +0300

    Update Main.java

--- src/Exhibition/Main.java
+++ src/Exhibition/Main.java
@@ -4,7 +4,6 @@ import Exhibition.gui.ExhibitionGUI;
 
 public class Main {
     public static void main(String[] args) {
-        // Use SwingUtilities.invokeLater to ensure thread safe GUI operations
         javax.swing.SwingUtilities.invokeLater(() -> {
             // Create and show the GUI
             ExhibitionGUI gui = new ExhibitionGUI();
